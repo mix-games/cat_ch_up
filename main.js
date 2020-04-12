8f69e018613b78163ae62228f5fc6effdedb1ec0
Yuki-jukjis <taka0618.m@gmail.com>
2020-04-13 02:08:32 +0900

    コーディング規約を少々

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 "use strict";
 window.onload = () => {
     const canvas = document.getElementById("canvas");
-    if (canvas == null || !(canvas instanceof HTMLCanvasElement)) {
+    if (canvas === null || !(canvas instanceof HTMLCanvasElement)) {
         alert("canvas not found");
         return;
     }
-    const context = canvas.getContext('2d');
-    if (context == null) {
+    const context = canvas.getContext("2d");
+    if (context === null) {
         alert("context2d not found");
         return;
     }
-    canvas.addEventListener('click', (ev) => {
+    canvas.addEventListener("click", (ev) => {
         const x = ev.clientX - canvas.offsetLeft;
         const y = ev.clientY - canvas.offsetTop;
     }, false);
