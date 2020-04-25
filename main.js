f49be61fc2e88494fe2020fa369e83d19a1f0172
Yuki-jukjis <taka0618.m@gmail.com>
2020-04-25 12:48:43 +0900

    よく分からん計算

--- main.js
+++ main.js
@@ -354,6 +354,9 @@ function createCamera() {
         // カメラ中心のスクリーン座標(移動アニメーション折り込み)
         centerX: 3 * blockSize,
         centerY: -1 * blockSize,
+        // カメラの移動速度
+        velocityX: 0,
+        velocityY: 0,
         // 描画用オフセット（スクリーン左上座標）
         offsetX: 0,
         offsetY: 0,
@@ -370,8 +373,15 @@ function updateCamera(camera, player, field, renderer) {
         camera.coord.y = player.coord.y - camera.clearanceY;
     const targetX = camera.coord.x * blockSize;
     const targetY = -camera.coord.y * blockSize;
-    camera.centerX += (targetX - camera.centerX) * 0.2;
-    camera.centerY += (targetY - camera.centerY) * 0.2;
+    const smooth = 0.9; // 1フレームあたりの減衰比(0～1の無次元値)
+    const accel = 1; // 1フレームあたりの速度変化
+    camera.velocityX =
+        Math.max(camera.velocityX * smooth - accel, Math.min(camera.velocityX * smooth + accel, // 減衰後の速度から±accellの範囲にのみ速度を更新できる
+        ((targetX - camera.centerX) * (1 - smooth)))); //この速度にしておけば公比smoothの無限級数がtargetXに収束する
+    camera.velocityY =
+        Math.max(camera.velocityY * smooth - accel, Math.min(camera.velocityY * smooth + accel, ((targetY - camera.centerY) * (1 - smooth))));
+    camera.centerX += camera.velocityX;
+    camera.centerY += camera.velocityY;
     camera.offsetX = Math.floor(renderer.lightColor.canvas.width / 2 - camera.centerX);
     camera.offsetY = Math.floor(renderer.lightColor.canvas.height / 2 - camera.centerY);
 }
--- main.ts
+++ main.ts
@@ -450,6 +450,9 @@ interface Camera {
 
     coord: Coord;
 
+    velocityX: number;
+    velocityY: number;
+
     centerX: number;
     centerY: number;
 
@@ -469,6 +472,10 @@ function createCamera(): Camera {
         // カメラ中心のスクリーン座標(移動アニメーション折り込み)
         centerX: 3 * blockSize,
         centerY: -1 * blockSize,
+        
+        // カメラの移動速度
+        velocityX: 0,
+        velocityY: 0,
 
         // 描画用オフセット（スクリーン左上座標）
         offsetX: 0,
@@ -488,9 +495,20 @@ function updateCamera(camera: Camera, player: Player, field: Field, renderer: Re
     
     const targetX = camera.coord.x * blockSize;
     const targetY = -camera.coord.y * blockSize;
+    const smooth = 0.9; // 1フレームあたりの減衰比(0～1の無次元値)
+    const accel = 1; // 1フレームあたりの速度変化
+
+    camera.velocityX = 
+        Math.max(camera.velocityX * smooth - accel,
+        Math.min(camera.velocityX * smooth + accel, // 減衰後の速度から±accellの範囲にのみ速度を更新できる
+            ((targetX - camera.centerX) * (1 - smooth))));  //この速度にしておけば公比smoothの無限級数がtargetXに収束する
+    camera.velocityY = 
+        Math.max(camera.velocityY * smooth - accel,
+        Math.min(camera.velocityY * smooth + accel,
+            ((targetY - camera.centerY) * (1 - smooth))));
     
-    camera.centerX += (targetX - camera.centerX) * 0.2;
-    camera.centerY += (targetY - camera.centerY) * 0.2;
+    camera.centerX += camera.velocityX;
+    camera.centerY += camera.velocityY;
     
     camera.offsetX = Math.floor(renderer.lightColor.canvas.width / 2 - camera.centerX);
     camera.offsetY = Math.floor(renderer.lightColor.canvas.height / 2 - camera.centerY);
